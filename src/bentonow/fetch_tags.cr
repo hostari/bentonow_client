62efe205e1e6693dc6c95082c44adcc1655f3a5d
paula4230 <paudepolonia000@yahoo.com>
2022-07-11 22:08:35 +0800

    Return an array of tags

--- src/bentonow/fetch_tags.cr
+++ src/bentonow/fetch_tags.cr
@@ -7,7 +7,7 @@ module Bentonow
       response = client.get("/api/v1/fetch/tags?site_uuid=#{site_uuid}")
 
       if response.status_code == 200
-        Array(Tag).from_json(response.body)
+        Hash(String, Array(Tag)).from_json(response.body)
       else
         raise Error.new(response.status_code.to_s)
       end
