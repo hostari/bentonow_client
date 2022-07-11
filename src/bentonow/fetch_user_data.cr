2b3a5ade839bab269351e6612d394f08c15c173c
paula4230 <paudepolonia000@yahoo.com>
2022-07-11 17:36:40 +0800

    Add endpoint to fetch user data and add Error class

---
+++ src/bentonow/fetch_user_data.cr
@@ -0,0 +1,17 @@
+# Returns a list of tags for each user account.
+
+module Bentonow
+  class Event
+    def self.fetch_user_data(publishable_key : String, secret_key : String, site_uuid : String, body : String)
+      client = Bentonow::Client.new(publishable_key, secret_key).reset_client
+      response = client.get("/api/v1/fetch/subscribers?site_uuid=#{site_uuid}", body: body)
+
+      if response.status_code == 200
+        pp response
+        JSON.parse(response.body)
+      else
+        raise Error.new(response.status_code.to_s)
+      end
+    end
+  end
+end
