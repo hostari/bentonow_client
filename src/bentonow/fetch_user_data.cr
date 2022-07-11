4ed29d8f7a3c8910cb93f4295e6acf58fd8d8e54
paula4230 <paudepolonia000@yahoo.com>
2022-07-11 17:45:58 +0800

    Remove response logger

--- src/bentonow/fetch_user_data.cr
+++ src/bentonow/fetch_user_data.cr
@@ -7,7 +7,6 @@ module Bentonow
       response = client.get("/api/v1/fetch/subscribers?site_uuid=#{site_uuid}", body: body)
 
       if response.status_code == 200
-        pp response
         JSON.parse(response.body)
       else
         raise Error.new(response.status_code.to_s)
